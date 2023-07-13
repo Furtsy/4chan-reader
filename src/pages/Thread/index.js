1893a001e3f7d7902d4bb17bf11eb4e086fc1b16
Furtsy <43937364+Furtsy@users.noreply.github.com>
2023-07-13 22:33:38 +0300

    cors button

--- src/pages/Thread/index.js
+++ src/pages/Thread/index.js
@@ -9,6 +9,8 @@ export function Thread() {
   const [posts, setPosts] = useState([]);
   const { board, threadId } = useParams();
 
+  const [showAccessButton, setShowAccessButton] = useState(false);
+
   useEffect(() => {
     const fetchThread = async () => {
       try {
@@ -37,17 +39,38 @@ export function Thread() {
 
         setPosts(fetchedPosts);
       } catch (error) {
-        console.error('pool closed asd famanas threasd!:', error);
+        console.error('pool closed asd famanas threasd:', error);
+      }
+    };
+
+    const checkAccess = async () => {
+      try {
+        const response = await axios.get('https://cors-anywhere.herokuapp.com/corsdemo');
+        if (response.status === 403) {
+          setShowAccessButton(true);
+        }
+      } catch (error) {
+        console.error('pool closed asd famanas access:', error);
       }
     };
 
     fetchThread();
+    checkAccess();
   }, [board, threadId]);
+
+  const handleAccessClick = () => {
+    window.location.href = 'https://cors-anywhere.herokuapp.com/corsdemo';
+  };
 
   return (
     <div className="bg-dark">
       <div className="container mx-auto p-4">
         <h1 className="text-3xl font-bold mb-4 text-white">4chan Reader</h1>
+        {showAccessButton && (
+          <div className="text-white mb-4">
+            If you can't see it, click <button className="text-blue-500 font-bold" onClick={handleAccessClick}>here</button> and activate "Request temporary access to the demo server".
+          </div>
+        )}
         <div className="grid grid-cols-3 gap-4">
           {posts.map((post, index) => (
             <div
